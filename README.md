e16d87e85ab6c9af0263f54bf78696c6d27fd6f8
Andrew Merrin <78210129+AM1010101@users.noreply.github.com>
2024-03-01 16:32:23 +0000

    Update UDP Publisher script and update README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # DOMIN_SW_INTERVIEW_TASK
+
+This document provides instructions on how to run the UDP Publisher. This script reads the csv file and send it lin by line to the UDP. Please feel free to use this to test injesting data as part of the interview task.
+
+## Prerequisites
+
+- Python (Tested with Python 3.10.8 on Windows 10)
+
+## Running the UDP Publisher
+
+1. Open a terminal.
+2. Navigate to the directory containing `udp_publisher.py`.
+3. Run the following command:
+
+```bash
+python udp_publisher.py
+```
--- udp_publisher.py
+++ udp_publisher.py
@@ -1,35 +1,32 @@
-'''
-this code loads the csv file and the publishees the data on a UDP port.
-this is intended to be used for mocking data from a real-time data source.
-'''
-
-'''
-example data:
-altitude,body_x_axis,body_y_axis,body_z_axis,fix,horizontal_dilution,latitude,longitude,num_sats,spd_over_grnd,timestamps,vehicle_accel_x,vehicle_accel_y,vehicle_accel_z,vehicle_gyro_x,vehicle_gyro_y,vehicle_gyro_z,vehicle_mag_x,vehicle_mag_y,vehicle_mag_z,vehicle_orientation_x,vehicle_orientation_y,vehicle_orientation_z,wheel_x_axis,wheel_y_axis,wheel_z_axis
-m,g,g,g,bool,count,deg,deg,count,km/h,s,m/s^2,m/s^2,m/s^2,deg/s,deg/s,deg/s,uT,uT,uT,deg,deg,deg,g,g,g
-100.41356206083556,-0.986163589,0.053049132227897644,0.17280970606952906,1,1,40.41356206083555,104.7189904,6,8.271241216711115,0.001,69.3,79.3,89.3,39.3,49.3,59.3,99.3,109.3,119.3,9.3,19.3,29.3,-0.039668731,-1.017069544,-0.07443793
-
-'''
-
 import csv
 import socket
+import time
+import logging
 
-# load the data
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
 data = []
 with open('sample_vehicle_data_6kph.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         data.append(row)
 
-# create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create a UDP socket
 
-# send the data
+start_time = time.time()
+data = data[2:] # revove the header
+
+logger.log(logging.INFO, 'sending data')
+
 for row in data:
+    while (float(row[10]) > time.time() - start_time): # check row ts is not in the future
+        logger.log(logging.INFO, {'waiting for':row[10]})
+        pass
     message = ','.join(row).encode()
     sock.sendto(message, ('localhost', 12345))
-    print('sent:', message)
+    logger.log(logging.INFO, {'sent:': message})
 
-# close the socket
 sock.close()
-
+logger.log(logging.INFO, 'done')
